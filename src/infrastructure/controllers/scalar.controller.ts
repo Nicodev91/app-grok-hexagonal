6c3b9c81d0a18e8319be99298b38605861744342
Nicolás Espinosa Carvajal <nicolas.sebasec@gmail.com>
2025-11-12 21:32:27 -0300

    feat:scalar is changed

--- src/infrastructure/controllers/scalar.controller.ts
+++ src/infrastructure/controllers/scalar.controller.ts
@@ -1,15 +1,4 @@
-// src/scalar.controller.ts
-import { Controller, Get, Query } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { Controller } from '@nestjs/common';
 
-@ApiTags('Scalar')
 @Controller('scalar')
-export class ScalarController {
-  @Get()
-  @ApiOperation({ summary: 'Suma dos números' })
-  @ApiQuery({ name: 'a', type: Number })
-  @ApiQuery({ name: 'b', type: Number })
-  sum(@Query('a') a: number, @Query('b') b: number): number {
-    return Number(a) + Number(b);
-  }
-}
+export class ScalarController {}
